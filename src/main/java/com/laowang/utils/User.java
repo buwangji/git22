91cd421591e4f5128323341db5e5cb5e05c315c7
buwangji <865837030@qq.com>
2016-12-06 10:44:04 +0800

    使用Ajax解析xml文件

---
+++ src/main/java/com/laowang/utils/User.java
@@ -0,0 +1,44 @@
+package com.laowang.utils;
+
+/**
+ * Created by Administrator on 2016/12/5.
+ */
+public class User {
+    private Integer id;
+    private String username;
+    private String address;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String address) {
+        this.id = id;
+        this.username = username;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
--- src/main/java/com/laowang/web/AjavServlet.java
+++ src/main/java/com/laowang/web/AjavServlet.java
@@ -1,4 +1,4 @@
-package com.laowang;
+package com.laowang.web;
 
 
 import javax.servlet.ServletException;
